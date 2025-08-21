657aab473a504bcb431d68f5babf48bff328033e
karu012 <karunyafrancis2003@gmail.com>
2025-08-21 23:54:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ def logout():
     session.pop('user_id', None)
     flash("You have been logged out.", 'success')
     return redirect(url_for('home'))
-
+    
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
 
